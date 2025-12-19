8321d09b369258a7b3b87c22576030446208aef6
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-19 22:36:40 +0530

    Implement authentication routing by adding SignIn and SignUp routes in App component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import LandingPage from './Components/LandingPage'
 import HomePage from './Components/HomePage'
+import SignUp from "./pages/auth/SignUp"
+import SignIn from "./pages/auth/SignIn"
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
-
+ 
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/home' element={<HomePage />}/>
+        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/signup" element={<SignIn />}/>
       </Routes>
     </main>
   )
