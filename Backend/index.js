7cb2d93c44390211fa5f7dd498cc9b37b6d69dcf
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-20 23:41:37 +0530

    Initialized backend with Express server and basic middleware

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,34 @@
-let express = require("express");
-let app = express();
-let path = require("path");
-let port = 3000;
+// let express = require("express");
+// let app = express();
+// let path = require("path");
+// let port = 3000;
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname,"public")));
+// app.use(express.urlencoded({extended: true}));
 
 
-app.listen(port,()=>console.log(`Server is running at htttp://localhost:${port}`));
+// app.listen(port,()=>console.log(`Server is running at htttp://localhost:${port}`));
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+
+const app = express()
+
+connectDB();
+
+app.use(cors());
+app.use(express.json())
+app.use("/api/auth", authRoutes);
+
+// const testRoute = require("./routes/test");
+// app.use("/test",testRoute)
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
