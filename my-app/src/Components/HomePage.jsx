348ca9f60e73f37b0b24c3ac4216329a86b83966
Shreyas Patil <shreyaspatil367@gmail.com>
2025-12-21 01:18:11 +0530

    Some Changes in HomePage

--- my-app/src/Components/HomePage.jsx
+++ my-app/src/Components/HomePage.jsx
@@ -1,24 +1,33 @@
 import "./HomePage.css";
+import { Routes,Route } from "react-router-dom";
+import Logo from "../assets/Logo-removebg-preview.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function HomePage(){
+    const [clicked,setClicked] = useState("false"); 
+
     return( <div className="HomeSection">
         <nav id="Main_Navigation"> 
             <div className="One">
-                <a href="/home"><img src="" alt="Logo" /></a>
+                <a href="/home"><img src={Logo} alt="Logo" /></a>
                 <h2>Swish</h2>
             </div>
             <div className="One">
-                <label for=""><i><i class="ri-search-line"></i></i></label>
+                <i className="ri-search-line"></i>
                 <input type="text" placeholder="Serach"/>
             </div>
         </nav>
         <nav id="SectionChanging">
-            <h3 onClick={()=>alert("working")}>Some</h3>
-            <h3 onClick={()=>alert("working")}>Some</h3>
-            <h3 onClick={()=>alert("working")}>Some</h3>
-            <h3 onClick={()=>alert("working")}>Some</h3>
+            <Link to="/home" className="LINKS">Home</Link>
+            <Link className="LINKS">About</Link>
+            <Link className="LINKS">Contact</Link>
         </nav>
         <div className="target"></div>
+        <section id="UserInteraction">
+            
+        </section>
+        
     </div>);
 }
 
