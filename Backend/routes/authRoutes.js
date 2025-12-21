3be29385406a0c48b0d96e425dfe0cf4d8008272
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-22 00:02:01 +0530

    Add protected auth route using JWT middleware

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const {signup,signin} = require("../controllers/authController");
+const protect = require("../middleware/authMiddleware");
+
+router.get("/me", protect, (req,res) => {
+    res.json({
+        message: "Protected route accessed",
+        user: req.user
+    });
+});
 
 router.post("/signup", signup);
 router.post("/signin", signin);
