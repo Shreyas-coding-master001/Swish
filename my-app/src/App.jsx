22ba429f7d2c50e17b9f2fe8840f79f13edd8428
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-24 21:44:50 +0530

    Created route for profile:

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from "./pages/auth/SignUp"
 import SignIn from "./pages/auth/SignIn"
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-
+import Profile from "./Components/Profile"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,7 +13,10 @@ function App() {
     <main>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />}>
+          <Route index element={<div />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
