25857317e4a98dbdc9858731d009ddfcae1192b5
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-31 13:32:13 +0530

    Added Feed as the default route inside HomePage

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,18 +7,22 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Profile from "./Components/Profile"
 import Community from './Components/Community'
-
+import PostUpload from './UI Components/PostUpload'
+import Card from "./pages/Post/Card"
+import Feed from "./Components/Feed"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <main>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />}>
-          <Route index element={<div />} />
+          <Route index element={<Feed />} />
           <Route path="profile" element={<Profile />}/>
           <Route path="community"element={<Community/>}/>
+          <Route path="card" element={<Card/>}/>
         </Route>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
