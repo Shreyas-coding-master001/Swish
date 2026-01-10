b95a0b844e9d1238e11cbf0e7186842e6aff3164
YashRaut24 <yashcodezzzz24@gmail.com>
2026-01-10 22:48:35 +0530

    Created mongoose schema for logs

---
+++ Backend/module/logs.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const logsSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, required: true, trim: true, ref: "users"},
+    name: {type: String, required: true, trim: true},
+    email: {type: String, required: true, trim: true}
+},{timestamps: true })
+
+module.exports = mongoose.model("logs",logsSchema);
